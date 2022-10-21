ff29b0fb42527e47fb21a09e024cc45aa2cc57cd
gusev76 <gusev76dn@mail.ru>
2022-10-21 21:46:28 +0300

    добавили папку WORK34

---
+++ WORK34/Program.cs
@@ -0,0 +1,33 @@
+﻿// задача 34
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Наш массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int a = 0; a < numbers.Length; a++)
+if (numbers[a] % 2 == 0)
+count++;
+
+Console.WriteLine($"Всего {numbers.Length} чисел, {count} из них четные числа");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
