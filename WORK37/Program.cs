a4d5f02d3a051f95b38f60529418f0008e336f0f
gusev76 <gusev76dn@mail.ru>
2022-10-21 22:09:20 +0300

    добавили папку WORK37

---
+++ WORK37/Program.cs
@@ -0,0 +1,32 @@
+﻿// задача 37
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Вот наш массив: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int a = 0; a < numbers.Length; a+=2)
+    sum = sum + numbers[a];
+
+    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечетных позициях = {sum}");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine("]");
+    Console.WriteLine();
+}
